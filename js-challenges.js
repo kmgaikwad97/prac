bc77293a02953ea562436fb4caa69f5c321aaa61
kshitijwohlig <kshitij.gaikwad@wohlig.com>
2023-11-01 11:49:48 +0530

    added readMe file

--- js-challenges.js
+++ js-challenges.js
@@ -15,6 +15,8 @@
 //   a++;
 // } while (a <= 10);
 
+// #2: Print the odd numbers less than 100
+
 // // even
 // for(let i=0; i<10; i+=2){
 //     console.log(i);
@@ -25,16 +27,35 @@
 //     console.log(i);
 // }
 
+// #3: Print the multiplication table with 7
+
 // for(let i=1; i<=10; i++){
 //     // console.log(`7 * ${i}`);
 //     let mul = `7 * ${i} = ` + 7 * i
 //     console.log(mul);
 // }
 
-for(let i=1; i<=10; i++){
-    for(let j=1; j<=10; j++){
-        // console.log("i ::",i ,"j ::", j);
-        const allTable =`${i} * ${j} = ` + i * j; 
-        console.log("allTable ::",allTable);
-    }
-}
+// #4: Print all the multiplication tables with numbers from 1 to 10
+// for(let i=1; i<=10; i++){
+//     for(let j=1; j<=10; j++){
+//         // console.log("i ::",i ,"j ::", j);
+//         const allTable =`${i} * ${j} = ` + i * j; 
+//         console.log("allTable ::",allTable);
+//     }
+// }
+
+// #5: Calculate the sum of numbers from 1 to 10
+// let a=null
+// for(let i=0;i<10;i++){
+//     a += i
+// }
+// console.log("a ::",a);
+
+// #6: Calculate 10! NOte :: = if 
+// let a = 1
+// for(let i = 1; i<10;i++){
+//     console.log(i);
+//     a  *= i;
+//     console.log("a ::",a );
+// }
+
---
+++ readMe.md
@@ -0,0 +1,13 @@
+# I'm Doing this coding challenge in js
+## To improve the logical understanding
+![Coding](https://images.unsplash.com/photo-1517694712202-14dd9538aa97?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)
+
+> Click Here for the question :
+[JS Coding Challenges](https://www.github.com)
+
+
+> I made this Readme file using this article : [readMe.md](https://medium.com/@saumya.ranjan/how-to-write-a-readme-md-file-markdown-file-20cb7cbcd6f)
+
+
+
+
